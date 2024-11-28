d3551d1ffe466d3bec5ab7327b13305f43af4c5a
Rodolfo <127708622+IvanRubisel@users.noreply.github.com>
2024-11-28 01:19:32 -0600

    test 2

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-    preset: 'jest-expo',
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-    transformIgnorePatterns: [
-      'node_modules/(?!react-native|@react-native|expo|@expo|@unimodules|unimodules)'
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!react-native|@react-native|expo|@expo|@unimodules|unimodules)',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  coverageDirectory: './coverage', // Genera reportes de cobertura
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./test-results",
+        outputName: "test-results.xml",
+      },
     ],
-    testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
-    coverageDirectory: './coverage', // Genera reportes de cobertura
-  };
-  
+  ],
+};
